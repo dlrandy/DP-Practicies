f25a0347bcaec05cf69c8ba65cb96dafa222a7f5
dlrandy <1208484996@qq.com>
2021-06-25 10:03:56 +0800

    feat: facade done

---
+++ structural/facade/Lecture/Subsystem2.ts
@@ -0,0 +1,9 @@
+export default class Subsystem2 {
+  public operation1():string {
+    return 'Subsystem2: Ready!\n';
+  }
+  public operationZ():string {
+    return 'Subsystem2: Go!\n';
+  }
+
+};
