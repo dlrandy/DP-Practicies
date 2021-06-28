996944c30cc8ffd2c5f718bb32c06165efee24ea
dlrandy <1208484996@qq.com>
2021-06-28 09:14:23 +0800

    feat: proxy review done

---
+++ structural/proxy/Exercise/Client.ts
@@ -0,0 +1,16 @@
+import RealSubject from "./RealSubject";
+import Subject from "./Subject";
+import Proxy from './Proxy'
+function clientCode(subject:Subject) {
+    subject.request()
+}
+
+console.log('Client: Executing the client code with a real subject:');
+const realSubject = new RealSubject();
+clientCode(realSubject);
+
+console.log('');
+
+console.log('Client: Executing the same client code with a proxy:');
+const proxy = new Proxy(realSubject);
+clientCode(proxy);
---
+++ structural/proxy/Exercise/Proxy.ts
@@ -0,0 +1,23 @@
+import RealSubject from "./RealSubject";
+import Subject from "./Subject";
+
+export default class Proxy implements Subject {
+   private realSubject:RealSubject;
+   constructor(realSubject:RealSubject) {
+       this.realSubject = realSubject;
+   }
+    public request(): void {
+        if (this.checkAccess()) {
+            this.realSubject.request();
+            this.logAccess();
+        }
+    }
+    private checkAccess():boolean {
+        console.log('Proxy: Checking access prior to firing a real request.');
+        return true;
+    }
+    private logAccess():void {
+        console.log('Proxy: Logging the time of request.');
+        
+    }
+};
