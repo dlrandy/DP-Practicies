90ba51887e06b5545b9a8349f2df536f6a97fd0a
dlrandy <1208484996@qq.com>
2021-06-29 11:35:20 +0800

    feat: abstract factory done

---
+++ creational/Abstract-Factory/Lecture/AbstractProductA.ts
@@ -0,0 +1,4 @@
+
+export default interface AbstractProductA{
+  usefulFunctionA():string;
+}
---
+++ creational/Abstract-Factory/Lecture/ConcreteProductA1.ts
@@ -0,0 +1,8 @@
+import AbstractProductA from "./AbstractProductA";
+
+export default class ConcreteProductA1 implements AbstractProductA {
+  public usefulFunctionA(): string {
+    return 'the result of the ConcreteProductA1.'
+  }
+  
+};
